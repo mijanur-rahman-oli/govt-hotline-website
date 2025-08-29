bd108b6fd96525ea4b20690f4219a5c0d4da7c5e
mijanur-rahman-oli <sheikhmijanurrahmanoli@gmail.com>
2025-08-29 22:25:14 +0600

    adding clipboard

--- home.js
+++ home.js
@@ -21,3 +21,36 @@ CountHeart("heart-btn-6");
 CountHeart("heart-btn-7");
 CountHeart("heart-btn-8");
 CountHeart("heart-btn-9");
+
+
+// copy function
+
+const copyAvailable = document.getElementById("copy")
+let copyCount = 2;
+
+function copyNumber(copyBtnId, numberId) {
+    const copyBtn = document.getElementById(copyBtnId);
+    const numberBtn = document.getElementById(numberId);
+    copyBtn.addEventListener("click", function () {
+        const number = numberBtn.textContent.trim();
+
+        navigator.clipboard.writeText(number).then(function () {
+            alert("Copied: " + number);
+            copyCount++;
+            copyAvailable.textContent = `${copyCount} Copy`;
+
+        })
+    })
+
+}
+
+
+copyNumber("copy-btn-1", "num-1");
+copyNumber("copy-btn-2", "num-2");
+copyNumber("copy-btn-3", "num-3");
+copyNumber("copy-btn-4", "num-4");
+copyNumber("copy-btn-5", "num-5");
+copyNumber("copy-btn-6", "num-6");
+copyNumber("copy-btn-7", "num-7");
+copyNumber("copy-btn-8", "num-8");
+copyNumber("copy-btn-9", "num-9");
